c38d41c4df71aef409c8df619d749d864744d24d
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-06-05 02:36:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,11 +4,11 @@
 board = ["X", " ", " ", " ", " ", " ", " ", " ", " ",]
 
 def display_board(board)
-    puts "#{           }"
+    puts "#{   |   |   }"
     puts "#{-----------}"
-    puts "#{           }"
+    puts "#{   |   |   }"
     puts "#{-----------}"
-    puts "#{           }"
+    puts "#{   |   |   }"
 end
 
 display_board(board)
