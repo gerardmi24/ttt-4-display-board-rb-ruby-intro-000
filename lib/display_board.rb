48948aa9b4c1fe87e257ead0de64a528b1bf5df1
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-06-05 02:25:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-
 def display_board
     puts "   |   |   "
     puts "-----------"
@@ -10,3 +8,5 @@ def display_board
     puts "-----------"
     puts "   |   |   "
 end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
