a4e210ee5519ac1acc03175dc76d16aa18a163ea
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-06-05 02:43:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def display_board(board)
+def display_board
     puts "#{   |   |   }"
     puts "#{-----------}"
     puts "#{   |   |   }"
